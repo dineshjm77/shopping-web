5e79c39505c8a8dc3acef23ece9c2346fbcfa165
dineshjm77 <132446052+dineshjm77@users.noreply.github.com>
2024-08-21 18:52:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var side=document.querySelector(".side-nav")
+function shownaw(){
+    
+    console.log("loading")
+    side.style.marginLeft="0px"
+    
+
+}
+function clousenaw()
+
+{
+    side.style.marginLeft="-60%"
+
+}
